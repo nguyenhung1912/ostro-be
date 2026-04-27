e0058f36477ce1661e7a89d69180b624ff43a4a3
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-04-27 23:54:34 +0700

    feat: implement signIn functionality with JWT and refresh token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import Session from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m";
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000;
 
 export const signUp = async (req, res) => {
   try {
@@ -32,7 +38,66 @@ export const signUp = async (req, res) => {
     // return
     return res.sendStatus(204);
   } catch (err) {
-    console.error("Failed to call signup", err);
+    console.error("Failed to call signUp", err);
+    return res.status(500).json({ message: "System error" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // get input from req
+    if (!username || !password) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    // get hashedPassword from DB to compare with input password
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // check password
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!passwordCorrect) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // if match, create access token with JWT
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: ACCESS_TOKEN_TTL },
+    );
+
+    // create refresh token
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    // create new session to store refresh token
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    // return refresh token in cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    // return access token in response
+    return res.status(200).json({
+      message: `User ${user.displayName} is logged in!`,
+      accessToken,
+    });
+  } catch (err) {
+    console.error("Failed to call signIn", err);
     return res.status(500).json({ message: "System error" });
   }
 };
