bbf4b7656bc710d4dc8890a76321157447236ac5
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-04-27 23:54:45 +0700

    feat: add session model for user authentication with refresh token

---
+++ src/models/Session.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// auto delete when expired
+sessionSchema.index({ expiresAt: 1 }, { expireAfterSecond: 0 });
+
+export default mongoose.model("Session", sessionSchema);
