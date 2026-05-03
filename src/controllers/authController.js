99a39603cefb02d27a95686a7a2b0964f275b1eb
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-03 16:03:38 +0700

    feat: implement refresh token functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import Session from "../models/Session.js";
 
-const ACCESS_TOKEN_TTL = "30m";
+const ACCESS_TOKEN_TTL = "30s";
 const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000;
 
 export const signUp = async (req, res) => {
@@ -118,6 +118,45 @@ export const signOut = async (req, res) => {
     return res.sendStatus(204);
   } catch (err) {
     console.error("Failed to call signOut", err);
+    return res.status(500).json({ message: "System error" });
+  }
+};
+
+// create new access token from refresh token
+export const refreshToken = async (req, res) => {
+  try {
+    // get refresh token from cookie
+    const token = req.cookie?.refreshToken;
+
+    if (!token) {
+      return res.status(401).json({ message: "Token does not exits" });
+    }
+
+    // compare refresh token in db
+    const session = await Session.findOne({ refreshToken: token });
+
+    if (!session) {
+      return res.status(403).json({ message: "Invalid Token or Expired" });
+    }
+
+    // check if it has expired
+    if (session.expiresAt < new Date()) {
+      return res.status(403).json({ message: "Token has expired." });
+    }
+
+    // create new access token
+    const accessToken = jwt.sign(
+      {
+        userId: session.userId,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: ACCESS_TOKEN_TTL },
+    );
+
+    // return
+    return res.status(200).json({ accessToken });
+  } catch (err) {
+    console.error("Failed to call refreshToken", err);
     return res.status(500).json({ message: "System error" });
   }
 };
