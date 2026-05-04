6581c5acdef1a6b0b1c05680d111616824ed7649
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-04 15:04:06 +0700

    feat: remove unused test route from userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,3 @@ export const authMe = async (req, res) => {
     return res.status(500).json({ message: "System error" });
   }
 };
-
-export const test = async (req, res) => {
-  return res.sendStatus(204);
-};
