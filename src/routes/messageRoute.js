521fe979a2a7172825db7f61ab29d636a9fd41a6
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-10 23:14:59 +0700

    feat: add checkFriendship middleware to direct message route

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -4,10 +4,11 @@ import {
   sendDirectMessage,
   sendGroupMessage,
 } from "../controllers/messageController.js";
+import { checkFriendship } from "../middlewares/friendMiddleware.js";
 
 const router = express.Router();
 
-router.post("/direct", sendDirectMessage);
+router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", sendGroupMessage);
 
 export default router;
