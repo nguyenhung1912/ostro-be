019ac3947d84ddc794c78299ee168a9f8995c0ca
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-11 15:36:33 +0700

    fix: correct function name and improve route definitions in conversation routes

--- src/routes/conversationRoute.js
+++ src/routes/conversationRoute.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
   createConversation,
-  getConversation,
+  getConversations,
   getMessages,
 } from "../controllers/conversationController.js";
-import { checkFriendShip } from "../middlewares/friendMiddleware.js";
+import { checkFriendship } from "../middlewares/friendMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", checkFriendShip, createConversation);
-router.post("/", getConversation);
-router.post("/:conversationId/messages", getMessages);
+router.post("/", checkFriendship, createConversation);
+router.get("/", getConversations);
+router.get("/:conversationId/messages", getMessages);
 
 export default router;
