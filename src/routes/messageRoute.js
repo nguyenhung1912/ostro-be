9679a4dd025470ac0048181e1d3229e0d19606ee
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-11 15:36:39 +0700

    fix: correct typo in friendship middleware function name in message routes

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,17 +1,16 @@
 import express from "express";
-
 import {
   sendDirectMessage,
   sendGroupMessage,
 } from "../controllers/messageController.js";
 import {
-  checkFriendShip,
+  checkFriendship,
   checkGroupMembership,
 } from "../middlewares/friendMiddleware.js";
 
 const router = express.Router();
 
-router.post("/direct", checkFriendShip, sendDirectMessage);
+router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", checkGroupMembership, sendGroupMessage);
 
 export default router;
