f0fc92c8bc11913ce15abed3f3334098daa7f180
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-11 16:12:43 +0700

    feat: add validation utility functions for checking valid ObjectIds

---
+++ src/utils/validation.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const isValidObjectId = (value) => mongoose.isValidObjectId(value);
+
+export const areValidObjectIds = (values) =>
+  Array.isArray(values) && values.every((value) => mongoose.isValidObjectId(value));
