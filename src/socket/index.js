016c58a82c6ff75430732c6ffb525505c82d9b0a
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-15 12:57:11 +0700

    feat: implement socket server with connection and disconnection handling

---
+++ src/socket/index.js
@@ -0,0 +1,25 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { log } from "console";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", async (socket) => {
+  console.log(`socket connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`socket disconnected: ${socket.id}`);
+  });
+});
+
+export { io, app, server };
