8b9a347531fc75677ac9a6acfc62d70ef2b595ca
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-19 18:33:07 +0700

    refactor: reorganize message route imports and add upload image and recall message routes

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,15 +1,20 @@
 import express from "express";
 import {
+  recallMessage,
   sendDirectMessage,
   sendGroupMessage,
+  uploadMessageImage,
 } from "../controllers/messageController.js";
 import {
   checkFriendship,
   checkGroupMembership,
 } from "../middlewares/friendMiddleware.js";
+import { upload } from "../middlewares/uploadMiddleware.js";
 
 const router = express.Router();
 
+router.post("/upload-image", upload.single("file"), uploadMessageImage);
+router.patch("/:messageId/recall", recallMessage);
 router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", checkGroupMembership, sendGroupMessage);
 
