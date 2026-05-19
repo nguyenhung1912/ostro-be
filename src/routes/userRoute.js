ebf5cb6507359d7f58b3c297736b5a99f3a9bdf5
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-19 18:33:26 +0700

    refactor: enhance user routes by adding update profile, change password, and delete account functionalities

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,22 @@
 import express from "express";
 import {
   authMe,
+  changePassword,
+  deleteAccount,
   searchUserByUsername,
   uploadAvatar,
+  uploadCover,
+  updateProfile,
 } from "../controllers/userController.js";
 import { upload } from "../middlewares/uploadMiddleware.js";
 const router = express.Router();
 
 router.get("/me", authMe);
+router.patch("/me", updateProfile);
+router.patch("/password", changePassword);
+router.delete("/me", deleteAccount);
 router.get("/search", searchUserByUsername);
 router.post("/uploadAvatar", upload.single("file"), uploadAvatar);
+router.post("/uploadCover", upload.single("file"), uploadCover);
 
 export default router;
