c4b712342a357dc751abc16b6fe67f5977cbad13
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-19 18:32:30 +0700

    refactor: add soft delete fields to message schema for improved message management

--- src/models/Message.js
+++ src/models/Message.js
@@ -21,6 +21,19 @@ const messageSchema = new mongoose.Schema(
       type: String,
       trim: true,
     },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+    deletedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
   },
   {
     timestamps: true,
